eb97ab5684ddced712648176ebccbce5b0b86eb5
ant313 <kaczyk13@gmail.com>
2019-07-23 22:51:28 +0200

    dodano pocję logowania

--- songbook/urls.py
+++ songbook/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('song/<int:pk>/', views.song_detail, name='song_detail'),
     path('song/new/', views.song_new, name='song_new'),
     path('song/<int:pk>/edit/', views.song_edit, name='song_edit'),
+    path('drafts/', views.song_draft_list, name='song_draft_list'),
+    path('song/<pk>/publish/', views.song_publish, name='song_publish'),
+    path('song/<pk>/remove/', views.song_remove, name='song_remove'),
 ]
--- songbook/views.py
+++ songbook/views.py
@@ -3,28 +3,30 @@ from django.utils import timezone
 from .models import Song
 from .forms import SongForm
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 def song_list(request):
-    songs = Song.objects.filter(published_date__lte=timezone.now()).order_by('title')
+    songs = Song.objects.filter(published_date__lte=timezone.now()).order_by('-title')
     return render(request, 'songbook/song_list.html', {'songs': songs})
 
 def song_detail(request, pk):
     song = get_object_or_404(Song, pk=pk)
     return render(request, 'songbook/song_detail.html', {'song': song})
 
+@login_required
 def song_new(request):
     if request.method == "POST":
         form = SongForm(request.POST)
         if form.is_valid():
             song = form.save(commit=False)
             song.author = request.user
-            song.published_date = timezone.now()
             song.save()
             return redirect('song_detail', pk=song.pk)
     else:
         form = SongForm()
     return render(request, 'songbook/song_edit.html', {'form': form})
 
+@login_required
 def song_edit(request, pk):
     song = get_object_or_404(Song, pk=pk)
     if request.method == "POST":
@@ -32,9 +34,25 @@ def song_edit(request, pk):
         if form.is_valid():
             song = form.save(commit=False)
             song.author = request.user
-            song.published_date = timezone.now()
             song.save()
             return redirect('song_detail', pk=song.pk)
     else:
         form = SongForm(instance=song)
     return render(request, 'songbook/song_edit.html', {'form': form})
+
+@login_required
+def song_draft_list(request):
+    songs = Song.objects.filter(published_date__isnull=True).order_by('title')
+    return render(request, 'songbook/song_draft_list.html', {'songs': songs})
+
+@login_required
+def song_publish(request, pk):
+    song = get_object_or_404(Song, pk=pk)
+    song.publish()
+    return redirect('song_detail', pk=pk)
+
+@login_required
+def song_remove(request, pk):
+    song = get_object_or_404(Song, pk=pk)
+    song.delete()
+    return redirect('song_list')
