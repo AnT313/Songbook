4d141c1bf7e8faa1d0431cca6ef77cf49c056a8f
ant313 <kaczyk13@gmail.com>
2019-07-23 23:17:25 +0200

    bugfix3?

--- śpiewnik/urls.py
+++ śpiewnik/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('songbook.urls')),
     path('accounts/login/', views.LoginView.as_view(), name='login')
-        path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
+
 ]
