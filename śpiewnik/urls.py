0f2f0d5a93f7393c94a4ec51172922038c6e4e85
ant313 <kaczyk13@gmail.com>
2019-07-23 23:11:42 +0200

    bugfix2

--- śpiewnik/urls.py
+++ śpiewnik/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('songbook.urls')),
     path('accounts/login/', views.LoginView.as_view(), name='login')
+        path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
