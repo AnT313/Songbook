55331da568fedc9d1c86f448ca334a16183764d6
ant313 <kaczyk13@gmail.com>
2019-07-23 20:27:11 +0200

    strona domowa i trochę css

--- songbook/views.py
+++ songbook/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Song
 
-# Create your views here.
+def song_list(request):
+    songs = Song.objects.filter(published_date__lte=timezone.now()).order_by('title')
+    return render(request, 'songbook/song_list.html', {'songs': songs})
