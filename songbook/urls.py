9bf92ff39a3332433129d1c8aa1b7de0ccdedfe2
ant313 <kaczyk13@gmail.com>
2019-07-23 21:57:38 +0200

    Opcja dodawania i edytowania piosenek + zabezpieczenia

--- songbook/urls.py
+++ songbook/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.song_list, name='song_list'),
     path('song/<int:pk>/', views.song_detail, name='song_detail'),
+    path('song/new/', views.song_new, name='song_new'),
+    path('song/<int:pk>/edit/', views.song_edit, name='song_edit'),
 ]
--- songbook/views.py
+++ songbook/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Song
+from .forms import SongForm
+from django.shortcuts import redirect
 
 def song_list(request):
     songs = Song.objects.filter(published_date__lte=timezone.now()).order_by('title')
@@ -9,3 +11,30 @@ def song_list(request):
 def song_detail(request, pk):
     song = get_object_or_404(Song, pk=pk)
     return render(request, 'songbook/song_detail.html', {'song': song})
+
+def song_new(request):
+    if request.method == "POST":
+        form = SongForm(request.POST)
+        if form.is_valid():
+            song = form.save(commit=False)
+            song.author = request.user
+            song.published_date = timezone.now()
+            song.save()
+            return redirect('song_detail', pk=song.pk)
+    else:
+        form = SongForm()
+    return render(request, 'songbook/song_edit.html', {'form': form})
+
+def song_edit(request, pk):
+    song = get_object_or_404(Song, pk=pk)
+    if request.method == "POST":
+        form = SongForm(request.POST, instance=song)
+        if form.is_valid():
+            song = form.save(commit=False)
+            song.author = request.user
+            song.published_date = timezone.now()
+            song.save()
+            return redirect('song_detail', pk=song.pk)
+    else:
+        form = SongForm(instance=song)
+    return render(request, 'songbook/song_edit.html', {'form': form})
